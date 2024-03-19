bdb73fe11d1fe5fe74d00e317d769591ab838236
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-03-19 18:49:03 -0500

    new changes

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 //
 public class Main {
+
+    public static void printItems(int n){
+        for(int i =0; i<=n; i++){
+            System.out.println("number: " + i);
+        }
+
+    }
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
-        }
+        printItems(10);
     }
 }
